86f48b6a0d28be6ad885c94c08d5b4a9325f9b7b
Denis <fpv.airplane.barnaul@gmail.com>
2024-11-23 16:17:24 +0300

    Initial Commit

---
+++ CreativeMobilesToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreativeMobilesToDo
+//
+//  Created by Denis on 23.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreativeMobilesToDo/CreativeMobilesToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeMobilesToDoApp.swift
+//  CreativeMobilesToDo
+//
+//  Created by Denis on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CreativeMobilesToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
