92d1a7231a66bff2dd1e09f52f9f707cbb343827
Denis <fpv.airplane.barnaul@gmail.com>
2025-01-27 23:36:24 +0300

    remove excess view

--- CreativeMobilesToDo/CreativeMobilesToDoApp.swift
+++ CreativeMobilesToDo/CreativeMobilesToDoApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CreativeMobilesToDoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            ToDoListView()
         }
     }
 }
